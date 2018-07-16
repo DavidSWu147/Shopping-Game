8af5c3f07f1db6f4581691f641a6963d530fe2a9
DavidSWu147 <35424731+DavidSWu147@users.noreply.github.com>
2018-07-16 13:24:01 -0700

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -2,13 +2,14 @@ import sys, pygame
 
 class MainMenu(object):  
 
-    def __init__(self):
+    def __init__(self, mC):
         pygame.init()
 
+        self.masterClass = mC
+
         self.displaySize = (900,600)
-        self.screen = pygame.display.set_mode(self.displaySize)
+        self.screen = pygame.Surface(self.displaySize)
         self.panelName = 'Main Menu'
-        pygame.display.set_caption(self.panelName)
 
         self.BLACK = (0,0,0)
         self.WHITE = (255,255,255)
@@ -16,17 +17,28 @@ class MainMenu(object):
 
     def __str__(self):
         return self.panelName
-
+    
+    '''
+    Don't call this method!
+    Will not work because screen has not been set using display.set_mode()
+    '''
     def run(self):
         self.draw()
+        pygame.display.update()
         
         self.hasExited = False
         while not self.hasExited:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.hasExited = True
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.curPos = pygame.mouse.get_pos()
+                    if 300 < self.curPos[0] and self.curPos[0] < 600 \
+                            and 300 < self.curPos[1] and self.curPos[1] < 400:
+                        self.hasExited = True
 
-            self.draw()            
+            self.draw() 
+            pygame.display.update()           
 
     def draw(self):
         self.screen.fill(self.WHITE)    
@@ -40,9 +52,11 @@ class MainMenu(object):
         self.text = self.fontMaster.render('Click Here to Enter',False,self.BLACK)
         self.screen.blit(self.text,(317,332))
 
-        pygame.display.update()
-            
+    def handleEvent(self,event):
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.curPos = pygame.mouse.get_pos()
+            if 300 < self.curPos[0] and self.curPos[0] < 600 \
+                        and 300 < self.curPos[1] and self.curPos[1] < 400:
+                return 'next screen'
 
-obj = MainMenu()
-obj.run()
-
+           
