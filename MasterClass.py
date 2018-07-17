85a4489cb83e846dda3cf17906016d644c6a1c9d
DavidSWu147 <35424731+DavidSWu147@users.noreply.github.com>
2018-07-17 12:13:01 -0700

    Add files via upload

--- MasterClass.py
+++ MasterClass.py
@@ -1,6 +1,7 @@
 import sys, pygame
 from MainMenu import MainMenu
 from StoreFront import StoreFront
+from InputDepartment import InputDepartment
 from InputBudget import InputBudget
 
 '''
@@ -24,16 +25,29 @@ class MasterClass(object):
         self.currentScreen = None 
 
         self.BLACK = (0,0,0)
+        self.DARK_GRAY = (64,64,64)
         self.GRAY = (128,128,128)
+        self.LIGHT_GRAY = (192,192,192)
         self.WHITE = (255,255,255)
         self.RED = (255,0,0)
         self.GREEN = (0,255,0)
-        self.BLUE = (0,0,255)        
+        self.BLUE = (0,0,255) 
+        self.YELLOW = (255,255,0)
+        self.CYAN = (0,255,255)
+        self.MAGENTA = (255,0,255)      
+
+        self.foodPrices = {'Bread':2,'Lemonade':3,'Milk':2,'Cereal':3,  \
+                'Orange Juice':2,'Apples':1,'Cake':7,'Lettuce':1,'Eggs':2}
+        self.homePrices = {'Wood':16,'Windows':40,'Wallpaper':12,'Paint':5, \
+                'Chair':28,'Photo Frame':20,'Lamp':32,'Candle':3,'Painting':8}
+        self.entertainmentPrices = {'Rocking Horse':14,'Chutes and Ladders':6,'Playing Cards':1,'Soccer Ball':5, \
+                'My First Fairy Tales':10,'Skateboard':9,'Frisbee':3,'Ping Pong Set':4,'Doll':4}
 
     def initializeClasses(self):
         self.mainMenu = MainMenu(self)
         self.storeFront = StoreFront(self)
-        self.inputBudget = InputBudget(self)
+        self.inputDepartment = InputDepartment(self)
+        self.inputBudget = InputBudget(self)       
 
     def __str__(self):
         return 'Master Class'
@@ -54,6 +68,9 @@ class MasterClass(object):
                     if command == 'Query':
                         self.currentClass = None
                         self.currentScreen = None
+                    elif command == 'InputDepartment':
+                        self.currentClass = self.inputDepartment
+                        self.currentScreen = self.inputDepartment.screen
                     elif command == 'InputBudget':
                         self.currentClass = self.inputBudget
                         self.currentScreen = self.inputBudget.screen
