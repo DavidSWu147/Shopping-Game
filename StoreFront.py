80cdb1c43051900ca39219d176cfc76ee10b43f2
DavidSWu147 <35424731+DavidSWu147@users.noreply.github.com>
2018-07-17 06:51:25 -0700

    Add files via upload

---
+++ StoreFront.py
@@ -0,0 +1,42 @@
+import sys, pygame
+
+class StoreFront(object):
+
+    def __init__(self, mC):
+        pygame.init()
+
+        self.masterClass = mC
+
+        self.displaySize = (900,600)
+        self.screen = pygame.Surface(self.displaySize)
+        self.panelName = 'Store Front'
+
+        self.BLACK = (0,0,0)
+        self.GRAY = (128,128,128)
+        self.WHITE = (255,255,255)
+        self.fontMaster = None
+
+    def __str__(self):
+        return self.panelName     
+
+    def draw(self):
+        self.screen.fill(self.WHITE)  
+
+        pygame.draw.rect(self.screen,self.BLACK,[100,100,700,400],5) 
+        pygame.draw.rect(self.screen,self.BLACK,[300,300,150,200],5)
+        pygame.draw.rect(self.screen,self.BLACK,[450,300,150,200],5)
+
+        self.fontMaster = pygame.font.SysFont('Calibri',128)
+        self.text = self.fontMaster.render('Store',False,(192,192,192))
+        self.screen.blit(self.text,(315,110))
+
+        self.fontMaster = pygame.font.SysFont('Calibri',64)
+        self.text = self.fontMaster.render('Entrance',False,self.BLACK)
+        self.screen.blit(self.text,(330,230))
+
+    def handleEvent(self,event):
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.curPos = pygame.mouse.get_pos()
+            if 300 < self.curPos[0] and self.curPos[0] < 600 \
+                        and 300 < self.curPos[1] and self.curPos[1] < 500:
+                return 'Query'
