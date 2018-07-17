09fe15358a00bdeac0e2ccea4481c577c17a1954
DavidSWu147 <35424731+DavidSWu147@users.noreply.github.com>
2018-07-17 07:52:21 -0700

    Add files via upload

--- InputBudget.py
+++ InputBudget.py
@@ -16,7 +16,7 @@ class InputBudget(object):
         self.WHITE = (255,255,255)
         self.fontMaster = None
 
-        self.initialBudget = 0.0
+        self.initialBudget = 0
 
     def __str__(self):
         return self.panelName        
@@ -25,17 +25,59 @@ class InputBudget(object):
         self.screen.fill(self.WHITE)
 
         pygame.draw.rect(self.screen,self.BLACK,[100,100,700,400],5) 
+        pygame.draw.rect(self.screen,self.BLACK,[200,300,500,100],2)
+
+        self.fontMaster = pygame.font.SysFont('Calibri',36)
+        self.text = self.fontMaster.render('Set Your Budget',False,self.BLACK)
+        self.screen.blit(self.text,(330,230))
 
         #Draw a back button
         pygame.draw.rect(self.screen,self.BLACK,[0,0,50,50],2)
         pygame.draw.polygon(self.screen,self.BLACK,[(40,10),(10,25),(40,40)],0)
+
+        #Draw the budget in dollars
+        self.fontMaster = pygame.font.SysFont('Calibri',64)
+        self.text = self.fontMaster.render('$' + str(self.initialBudget),False,self.BLACK)
+        self.screen.blit(self.text,(220,320))
 
     def handleEvent(self,event):
         if event.type == pygame.MOUSEBUTTONUP:
             self.curPos = pygame.mouse.get_pos()
             if 0 < self.curPos[0] and self.curPos[0] < 50 \
                         and 0 < self.curPos[1] and self.curPos[1] < 50:
+                self.initialBudget = 0
                 return 'StoreFront'
+        elif event.type == pygame.KEYUP:
+            self.curKey = event.key
+
+            if self.curKey == pygame.K_RETURN:
+                return 'Query'
+            elif self.curKey == pygame.K_BACKSPACE:
+                self.initialBudget = 0
+            elif self.initialBudget < 100:
+                if self.curKey == pygame.K_1:    
+                    self.initialBudget = self.initialBudget * 10 + 1
+                elif self.curKey == pygame.K_2:
+                    self.initialBudget = self.initialBudget * 10 + 2
+                elif self.curKey == pygame.K_3:
+                    self.initialBudget = self.initialBudget * 10 + 3
+                elif self.curKey == pygame.K_4:
+                    self.initialBudget = self.initialBudget * 10 + 4
+                elif self.curKey == pygame.K_5:
+                    self.initialBudget = self.initialBudget * 10 + 5
+                elif self.curKey == pygame.K_6:
+                    self.initialBudget = self.initialBudget * 10 + 6
+                elif self.curKey == pygame.K_7:
+                    self.initialBudget = self.initialBudget * 10 + 7
+                elif self.curKey == pygame.K_8:
+                    self.initialBudget = self.initialBudget * 10 + 8
+                elif self.curKey == pygame.K_9:
+                    self.initialBudget = self.initialBudget * 10 + 9
+                elif self.curKey == pygame.K_0:
+                    self.initialBudget = self.initialBudget * 10 
+
+        self.draw()
+
         
 
     
